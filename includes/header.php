67a7eaaeb9e7a46178b344dea31934affe62ad15
Jim Munro <jamescmunro84@gmail.com>
2011-08-04 23:41:07 -0400

    added css basics

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,13 @@
 <!doctype html>
 <html>
 <head>
-	<title><?php echo $title; ?></title>
+
+<title><?php echo $title; ?></title>
+
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/960.css" />
+<link rel="stylesheet" href="css/main.css" />
+
 </head>
 <body>
 <!-- end header.php -->
